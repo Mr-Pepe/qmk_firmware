c5c60ecfcb31cb1072fa7a4b7dc44391cde68ead
Felipe Peter <mr-peipei@web.de>
2022-12-29 21:25:00 +0100

    Move to new kyria folder

--- keyboards/splitkb/kyria/keymaps/Mr-Pepe/keymap.c
+++ keyboards/splitkb/kyria/keymaps/Mr-Pepe/keymap.c
@@ -14,9 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+// #include "process_key_override.h"
+
 
 enum layers {
-    _QWERTY = 0,
+    _NEO = 0,
     _ARROWS,
     _WINDOWING,
     _NUMBERS,
@@ -39,11 +41,21 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT(
-      KC_BSPC,       KC_Q,                     KC_W,  KC_E,           KC_R,       KC_T,                                                                                                   KC_Z,       KC_U,       KC_I,    KC_O,    KC_SCLN,     KC_LBRC,
-      MT(MOD_LSFT, KC_TAB),                 KC_A,  LT(_SYMBOLS, KC_S),       MT(MOD_LSFT, KC_D),           KC_F,       KC_G,                                                                                                   KC_H,       KC_J, MT(MOD_LSFT, KC_K),    LT(_SYMBOLS, KC_L),    KC_P,  KC_QUOT,
-      MT(MOD_LCTL, S(KC_TAB)),                KC_Y,  KC_X,           LT(_ARROWS, KC_C),           KC_V,       KC_B,                   KC_PSCR,            KC__VOLDOWN,            KC__VOLUP,            KC_MPLY,            KC_N,       KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                                                        KC_LGUI,        LT(_WINDOWING, KC_LGUI),   MT(MOD_LCTL, KC_ENT),   MT(MOD_LALT, KC_SPC), LT(_FN, KC_ESC),   LT(_FN, KC_ESC), LT(_NUMBERS, KC_SPC), MO(_ARROWS),     KC_LSFT,    KC_RALT
+  //  * ,-------------------------------------------.                              ,-------------------------------------------.
+  //  * |        |   Ü  |  Ö   |   Y  |  P   |  W   |                              |   K  |   H  |  G   |   F  |   Q  |   Ä    |
+  //  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+  //  * |        |   U  |  I   |   A  |  E   |  O   |                              |   S  |   N  |  R   |   T  |   ,  |   -    |
+  //  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+  //  * |        |   X  |  V   |   J  |  C   |   Z  |      |      |  |      |      |   B  |   M  |  L   |   D  |   .  |   ß    |
+  //  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+  //  *                        |      |      |      |      |      |  |      |      |      |      |      |
+  //  *                        |      |      |      |      |      |  |      |      |      |      |      |
+  //  *                        `----------------------------------'  `----------------------------------'
+    [_NEO] = LAYOUT(
+      KC_BSPC,                KC_LBRC,      KC_SCLN,  KC_Z,           KC_P,       KC_W,                                                                                                   KC_K,       KC_H,       KC_G,    KC_F,    KC_Q,     KC_QUOT,
+      KC_TAB,    MT(MOD_LSFT, KC_U),  LT(_SYMBOLS, KC_I),       MT(MOD_LSFT, KC_A),           KC_E,       KC_O,                                                                KC_S,       KC_N, MT(MOD_LSFT, KC_R),    LT(_SYMBOLS, KC_T),    KC_COMM,  KC_SLSH,
+      S(KC_TAB), MT(MOD_LCTL, KC_X),  KC_V,   KC_J,   KC_C,   KC_Y,    KC_PSCR,            KC__VOLDOWN,            KC__VOLUP,            KC_MPLY,            KC_B,       KC_M,       KC_L, KC_D,  KC_DOT, KC_MINS,
+                                                        C(KC_S),        LT(_WINDOWING, KC_LGUI),   MT(MOD_LCTL, KC_ENT),   MT(MOD_LALT, KC_SPC), LT(_FN, KC_ESC),   LT(_FN, KC_ESC), LT(_NUMBERS, KC_SPC), LT(_ARROWS, KC_ENT),     KC_LSFT,    KC_RALT
     ),
     [_ARROWS] = LAYOUT(
       _______, _______, _______, _______, _______,    _______,                                     _______, _______, KC_UP,   _______,    _______,    _______,
@@ -61,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______,                                     _______, KC_7,    KC_8,    KC_9, _______, _______,
       _______, _______, _______, SQR, QUB, _______,                                     KC_0,    KC_4,    KC_5,    KC_6, C(KC_P), A(S(KC_P)),
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, KC_LCTL, KC_LALT, _______, _______, _______, _______, _______, _______
     ),
     [_SYMBOLS] = LAYOUT(
       _______, S(KC_GRV), EUR, S(KC_4), S(KC_5), S(KC_6),                                     S(KC_RBRC), OSB,     CSB,     S(KC_0),    KC_NUHS, KC_EQL,
@@ -73,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_F11, KC_F12, _______, _______,    _______,                                     _______, _______, _______, _______, _______, _______,
       _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                                     _______, _______, _______, _______, _______, _______,
       _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, KC_BRIGHTNESS_DOWN,  KC_BRIGHTNESS_UP,  KC__MUTE, _______, _______, _______, _______, _______, _______,
-                                 _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, KC_MPRV,    KC_MNXT, _______, _______, _______, _______, _______, _______, _______
     ),
 // /*
 //  * Layer template
@@ -153,6 +165,14 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return TAPPING_TERM;
     }
 }
+
+// const key_override_t ctrl_s = ko_make_basic(MOD_MASK_CTRL, KC_A, C(KC_S));
+
+// // This globally defines all key overrides to be used
+// const key_override_t **key_overrides = (const key_override_t *[]){
+// 	&ctrl_s,
+// 	NULL // Null terminate the array of overrides!
+// };
 
 // layer_state_t layer_state_set_user(layer_state_t state) {
 //     return update_tri_layer_state(state, _ARROWS);
